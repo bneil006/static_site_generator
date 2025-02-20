4ee5702e7cb0a1a1a3bb12cc9074f8bf24d7571c
bneil006 <brmneil2@gmail.com>
2025-02-20 03:55:23 -0600

    added quite a bit more

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,16 +5,6 @@ class HTMLNode:
         self.value = value # An HTMLNode without a value will be assumed to have children
         self.children = children # An HTMLNode without children will be assumed to have a value
         self.props = props # An HTMLNode without props simply won't have any attributes
-
-    def __eq__(self, other):
-        return (
-            self.tag == other.tag
-            and self.value == other.value
-            and self.children == other.children
-            and self.props == other.props
-        )
-    def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
     def to_html(self):
         raise NotImplementedError
@@ -25,6 +15,16 @@ class HTMLNode:
         for i in self.props:
             formatted += f' {i}="{self.props[i]}"'
         return formatted
+    
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.children == other.children
+            and self.props == other.props
+        )
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 # A LeafNode is a type of HTMLNode that represents a single HTML tag with no children. For example, a simple <p> tag with some text inside of it
 class LeafNode(HTMLNode):
@@ -35,14 +35,47 @@ class LeafNode(HTMLNode):
         if self.value is None: raise ValueError("invalid HTML: no value")
         if self.tag is None: return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+# ParentNode class will handle the nesting of HTML nodes inside of one another. Any HTML node that's not a LeafNode (i.e. it has children) is a ParentNode
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None): # has children, no value
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None: raise ValueError("Must have tag")
+        if self.children is None: raise ValueError("Must have children")
+
+        children_html = ""
+        for child in self.children: # iterating through LeafNode / Children of ParentNode
+                children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
+
 
 
 
 def something():
     html_node = HTMLNode("p", "this is the text", "probably replace later with a class object", None)
+
     a = LeafNode("p", "This is a paragraph of text.")
     b = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    print(a.to_html())
-    print(b.to_html())
+    # print(a.to_html())
+    # print(b.to_html())
+
+    list_for_parent_node = [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+        ]
+    
+    c = ParentNode("p", list_for_parent_node)
+    print(c.to_html())
 
 something()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 def main():
-    node = TextNode("Sometext", TextType.BOLD, "https://www.boot.dev")
-    node2 = HTMLNode("p", "this is the text", "probably replace later with a class object", "proppa")
-    print(node) # can print the Class like this since we have the __repr__ set up for this class
-    print(node2)
+    pass
 
 if __name__ == "__main__":
     main()
